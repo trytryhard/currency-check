00ca0ae3a60a35270fb3d1cd1638aea45b17d978
trytryhard <aleiiia31@gmail.com>
2025-07-24 15:18:34 +1000

    returnin from long break period

--- currencylist.py
+++ currencylist.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 import re
+#import xml.etree.ElementTree as ET
 import time
-
+import yaml
 '''
 todo:
 1) making list of currencies kinda-json =>
@@ -33,8 +34,14 @@ unixtime : {BANK1:
 '''
 
 class CurrencyListCBRF: # <div class="table-wrapper"> tbody /tbody
+    '''
+    Gettin currency dict for CBRF
+    '''
+
     @staticmethod
     def getData()->dict:
+        '''get data of currency(shortName), nameCurr(fullName), volumeOfCurr'''
+
         url = f'https://www.cbr.ru/currency_base/daily/?UniDbQuery.Posted=True&UniDbQuery.To='
         options = webdriver.ChromeOptions()
         options.page_load_strategy = 'eager'
@@ -52,18 +59,22 @@ class CurrencyListCBRF: # <div class="table-wrapper"> tbody /tbody
 
         for row in htmlVar.split('/tr')[1:-1:]:
             shortName = re.findall('[A-Z]{3}',row)[0]
-            volume = re.findall('\d+',row)[1]
+            volume = int(re.findall('\d+',row)[1])
             longName = ' '.join(re.findall('[А-Яа-я]+',row))
             resDict['CBRF'].append({"currency": shortName,
-                "nameCurr":longName,
+                "nameCurr":longName, #.encode('utf-16'),
                 "volumeOfCurr":volume})
 
         return resDict
 
 class CurrencyListSolid:
+    '''
+    Gettin currency dict for CBRF
+    '''
     @staticmethod
     def getData()->dict:
-        url = f'https://solidbank.ru/currency-transactions/?location=%D0%A5%D0%B0%D0%B1%D0%B0%D1%80%D0%BE%D0%B2%D1%81%D0%BA'
+        '''get data of currency(shortName), nameCurr(fullName), volumeOfCurr'''
+        url = f'https://solidbank.ru/currency-transactions/?location=%D0%A5%D0%B0%D0%B1%D0%B0%D1%80%D0%BE%D0%B2%D1%81%D0%BA' # "Хабаровск"
         options = webdriver.ChromeOptions()
         options.page_load_strategy = 'eager'
         options.add_argument('--ignore-ssl-errors=yes')
@@ -83,7 +94,7 @@ class CurrencyListSolid:
             shortName = re.findall('[A-Z]{3}',row)[0]
 
             try:
-                volume = re.findall('\d+',row.split('</span>')[0])[0]
+                volume = int(re.findall('\d+',row.split('</span>')[0])[0])
             except:
                 volume = 1
 
@@ -95,4 +106,60 @@ class CurrencyListSolid:
 
         return resDict
 
-print(CurrencyListSolid.getData())
+class WorkWithFile:
+    '''
+    class of worin with file R/W +check value in yaml
+    '''
+    def __init__(self,bankShortName):
+        self.bankName=bankShortName
+
+    def checkDateOfFile(self):
+        '''def return True if current month in currencyList file else False -> update file with bank currencies'''
+        pass
+        with open('currencylist.yaml','r') as yamlFile:
+            dictOutOfYaml = yaml.safe_load(yamlFile)
+            return True if time.strftime('%m-%Y') == list(dictOutOfYaml.keys())[0] else False
+
+    def checkBankInFile(self):
+        '''def return True if bankName match with banks in currencyList else False -> update file with bank currencies'''
+        pass
+        with open('currencylist.yaml','r') as yamlFile:
+            dictOutOfYaml = yaml.safe_load(yamlFile)
+            return True if (self.bankName in list( x[0] for x in  list( list(x.keys()) for x in (dictOutOfYaml[ list(dictOutOfYaml.keys())[0] ]) ))) else False
+
+    def writeFile(self):
+        '''def check by funcs checkBankInFile, checkDateOfFile then update currencyList file
+        yaml template: Bank -> currency(short english name), description, volume
+
+        need to add: check bank in yaml, if in it then
+        '''
+        #if self.checkDateOfFile() == self.checkBankInFile() and self.checkBankInFile() == True:
+            #print('Same month flag')
+            #return True
+
+        #checkin date for
+        print('Same month flag') if self.checkDateOfFile() == True else print('Outdated file')
+
+        #needs to check in same month entity of bankName
+
+        resDict = {time.strftime('%m-%Y'):[]}
+
+        resDict[list(resDict.keys())[0]].append(CurrencyListCBRF.getData())
+        resDict[list(resDict.keys())[0]].append(CurrencyListSolid.getData())
+
+        #with open('currencylist.yaml', 'w') as yamlFile:
+            #yaml.dump(resDict, yamlFile)#, allow_unicode=True)
+
+        filePath = 'currencylist.yaml'
+        try:
+            with open(filePath, 'w',  encoding='utf-16') as yamlFile:
+                yaml.dump(resDict, yamlFile, default_flow_style=False, sort_keys=False,  allow_unicode=True)
+            print(f"Dictionary successfully saved to {filePath}")
+        except Exception as e:
+            print(f"Error occurred while saving YAML file: {e}")
+
+        #return resDict
+        return True
+
+print(WorkWithFile('CBRF').writeFile())
+#print(CurrencyListCBRF.getData())
